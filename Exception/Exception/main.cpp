46aea2f004c05e15326368eba7df184e0378a1f7
Federico Virgili <federico.virgili@gmail.com>
2020-01-01 16:19:47 +0100

    Exception project

---
+++ Exception/Exception/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Exception
+//
+//  Created by Federico Virgili on 01/01/20.
+//  Copyright © 2020 Federico Virgili. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+    
+}
